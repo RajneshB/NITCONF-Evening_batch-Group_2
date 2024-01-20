118bf8a4b393858e42edf9799db341d185a50da3
Johann <jbsimon03@gmail.com>
2024-01-20 15:51:34 +0530

    initialized

---
+++ Frontend/src/pages/Profile/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const Profile = () =>{
+    return(
+        <div className="profileContainer">
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Profile;
