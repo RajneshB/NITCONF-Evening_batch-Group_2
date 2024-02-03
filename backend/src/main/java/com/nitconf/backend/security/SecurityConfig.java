10d77331d70c8b4405229cfe6d3ad0af0b636e9a
RajneshB <rajneshbaiju@gmail.com>
2024-02-03 08:25:55 +0530

    supressing null

--- backend/src/main/java/com/nitconf/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/nitconf/backend/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.nitconf.backend.security;
 
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
