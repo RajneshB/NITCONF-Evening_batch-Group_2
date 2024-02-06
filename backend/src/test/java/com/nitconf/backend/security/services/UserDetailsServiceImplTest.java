8bab82f7f5f9d56dc00a5f088d14a19504f9fc2a
RajneshB <rajneshbaiju@gmail.com>
2024-02-06 12:15:39 +0530

    finishing userdetailsservicetest

--- backend/src/test/java/com/nitconf/backend/security/services/UserDetailsServiceImplTest.java
+++ backend/src/test/java/com/nitconf/backend/security/services/UserDetailsServiceImplTest.java
@@ -10,9 +10,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.nitconf.backend.models.User;
 import com.nitconf.backend.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataMongoTest
@@ -26,16 +29,27 @@ public class UserDetailsServiceImplTest {
 
 
     @Test
-    void testLoadUserByUsername() {
-        String email="testing@example.com";
+    public void testLoadUserByUsername() {
+        String username="testing@example.com";
         User user= new User();
-        user.setEmail(email);
-        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        user.setEmail(username);
+        when(userRepository.findByEmail(username)).thenReturn(Optional.of(user));
 
-        UserDetails userDetails=userDetailsService.loadUserByUsername(email);
+        UserDetails userDetails=userDetailsService.loadUserByUsername(username);
 
         assertTrue(userDetails.isEnabled());
-        verify(userRepository,times(1)).findByEmail(email);
+        verify(userRepository,times(1)).findByEmail(username);
         
+    }
+    @Test
+    public void testLoadUserByUsername_UserNotFound(){
+        String username = "nonexistent@example.com";
+        when(userRepository.findByEmail(username)).thenReturn(Optional.empty());
+
+
+        assertThrows(UsernameNotFoundException.class, () -> userDetailsService.loadUserByUsername(username));
+
+        verify(userRepository, times(1)).findByEmail(username);
+
     }
 }
