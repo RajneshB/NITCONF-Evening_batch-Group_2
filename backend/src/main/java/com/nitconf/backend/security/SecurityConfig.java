4979adfd22dd07a437ea1729a4b59678b891db43
RajneshB <rajneshbaiju@gmail.com>
2024-02-12 22:22:13 +0530

    adding swagger ui

--- backend/src/main/java/com/nitconf/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/nitconf/backend/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/**").permitAll()
+          auth.requestMatchers("/api/auth/**", "/api/docs/**").permitAll()
               .requestMatchers("/api/paper/**").permitAll()
               .requestMatchers("/api/forgotPassword/**").permitAll()
               .anyRequest().authenticated()
