11a55a0d3018d9d72efbe569a62d22103d33a38a
jj58dev <joel.joseph5803@gmail.com>
2024-04-08 09:36:43 +0530

    initial

--- backend/src/main/java/com/nitconf/backend/models/Paper.java
+++ backend/src/main/java/com/nitconf/backend/models/Paper.java
@@ -62,14 +62,18 @@ public class Paper {
     }
 
     public void setRating() {
+        
         if (reviews != null && !reviews.isEmpty()) {
-            int sum = 0;
+            double totalScore = 0;
+            int criteriaCount = 0;
+
             for (Review review : reviews) {
-                if (review.getRating() != null) {
-                    sum += review.getRating();
-                }
+                review.setOverallScore();
+                totalScore += review.getOverallScore();
+                criteriaCount++;
             }
-            this.rating = sum / reviews.size();
+
+            this.rating = (int) Math.round(totalScore / criteriaCount);
         } else {
             this.rating = null;
         }
@@ -116,45 +120,137 @@ public class Paper {
     }
 
     public static class Review {
-
         private String reviewer;
         private String comments;
-        private Integer rating;
+        private Integer originalityScore;
+        private Integer significanceScore;
+        private Integer methodologyScore;
+        private Integer clarityAndOrganizationScore;
+        private Integer literatureReviewScore;
+        private Integer resultsAndDiscussionScore;
+        private Integer referencesAndCitationsScore;
+        private Integer ethicalConsiderationsScore;
         private String reviewerDecision;
-
-        public Review(String reviewer, String comments, Integer rating, String reviewerDecision) {
+        private Integer overallScore;
+    
+        public Review(String reviewer, String comments, Integer originalityScore, Integer significanceScore,
+                        Integer methodologyScore, Integer clarityAndOrganizationScore, Integer literatureReviewScore,
+                        Integer resultsAndDiscussionScore, Integer referencesAndCitationsScore,
+                        Integer ethicalConsiderationsScore, String reviewerDecision) {
             this.reviewer = reviewer;
             this.comments = comments;
-            this.rating = rating;
-            this.reviewerDecision = reviewerDecision;
-        }
-        
-        public String getComments() {
-            return comments;
-        }
-        public void setComments(String comments) {
-            this.comments = comments;
-        }
-        public Integer getRating() {
-            return rating;
-        }
-        public void setRating(Integer rating) {
-            this.rating = rating;
-        }
-        public String getReviewerDecision() {
-            return reviewerDecision;
-        }
-        public void setReviewerDecision(String reviewerDecision) {
+            this.originalityScore = originalityScore;
+            this.significanceScore = significanceScore;
+            this.methodologyScore = methodologyScore;
+            this.clarityAndOrganizationScore = clarityAndOrganizationScore;
+            this.literatureReviewScore = literatureReviewScore;
+            this.resultsAndDiscussionScore = resultsAndDiscussionScore;
+            this.referencesAndCitationsScore = referencesAndCitationsScore;
+            this.ethicalConsiderationsScore = ethicalConsiderationsScore;
             this.reviewerDecision = reviewerDecision;
         }
 
+        public void setOverallScore() {
+            int totalScores = originalityScore + significanceScore + methodologyScore +
+                    clarityAndOrganizationScore + literatureReviewScore + resultsAndDiscussionScore +
+                    referencesAndCitationsScore + ethicalConsiderationsScore;
+            int criteriaCount = 8;
+    
+            overallScore = totalScores / criteriaCount;
+        }
+
+        public Integer getOverallScore() {
+            return overallScore;
+        }
+        
+    
+        // Getters and Setters
         public String getReviewer() {
             return reviewer;
         }
-
+    
         public void setReviewer(String reviewer) {
             this.reviewer = reviewer;
         }
-        
+    
+        public String getComments() {
+            return comments;
+        }
+    
+        public void setComments(String comments) {
+            this.comments = comments;
+        }
+    
+        public Integer getOriginalityScore() {
+            return originalityScore;
+        }
+    
+        public void setOriginalityScore(Integer originalityScore) {
+            this.originalityScore = originalityScore;
+        }
+    
+        public Integer getSignificanceScore() {
+            return significanceScore;
+        }
+    
+        public void setSignificanceScore(Integer significanceScore) {
+            this.significanceScore = significanceScore;
+        }
+    
+        public Integer getMethodologyScore() {
+            return methodologyScore;
+        }
+    
+        public void setMethodologyScore(Integer methodologyScore) {
+            this.methodologyScore = methodologyScore;
+        }
+    
+        public Integer getClarityAndOrganizationScore() {
+            return clarityAndOrganizationScore;
+        }
+    
+        public void setClarityAndOrganizationScore(Integer clarityAndOrganizationScore) {
+            this.clarityAndOrganizationScore = clarityAndOrganizationScore;
+        }
+    
+        public Integer getLiteratureReviewScore() {
+            return literatureReviewScore;
+        }
+    
+        public void setLiteratureReviewScore(Integer literatureReviewScore) {
+            this.literatureReviewScore = literatureReviewScore;
+        }
+    
+        public Integer getResultsAndDiscussionScore() {
+            return resultsAndDiscussionScore;
+        }
+    
+        public void setResultsAndDiscussionScore(Integer resultsAndDiscussionScore) {
+            this.resultsAndDiscussionScore = resultsAndDiscussionScore;
+        }
+    
+        public Integer getReferencesAndCitationsScore() {
+            return referencesAndCitationsScore;
+        }
+    
+        public void setReferencesAndCitationsScore(Integer referencesAndCitationsScore) {
+            this.referencesAndCitationsScore = referencesAndCitationsScore;
+        }
+    
+        public Integer getEthicalConsiderationsScore() {
+            return ethicalConsiderationsScore;
+        }
+    
+        public void setEthicalConsiderationsScore(Integer ethicalConsiderationsScore) {
+            this.ethicalConsiderationsScore = ethicalConsiderationsScore;
+        }
+    
+        public String getReviewerDecision() {
+            return reviewerDecision;
+        }
+    
+        public void setReviewerDecision(String reviewerDecision) {
+            this.reviewerDecision = reviewerDecision;
+        }
     }
 }
